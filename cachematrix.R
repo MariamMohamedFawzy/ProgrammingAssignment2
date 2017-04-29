21d915470c23a40f60dbca5ae91a6722ed61fa4b
Mariam <maryammohamed61@gmail.com>
2017-04-29 08:34:25 +0200

    first commit : solve the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+require(MASS)
+
 ## Write a short comment describing this function
-
+#this function creates a matrix and caches its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) m <<- inv
+  getInverse <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
 ## Write a short comment describing this function
+#this function checks that the calculated value and the cached one are equal
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- ginv(data)
+  x$setInverse(m)
+  m
 }
